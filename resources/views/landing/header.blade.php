a2430deede2e12d8712f14223637a33bdcd9c0db
unknown <imronfajar0902@gmail.com>
2023-01-27 00:59:26 +0700

    first

---
+++ resources/views/landing/header.blade.php
@@ -0,0 +1,21 @@
+<div class="container-fluid hero-header bg-light py-5 mb-5">
+    <div class="container py-5">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6">
+                <h1 class="display-4 mb-3 animated slideInDown">Repository Kerja Praktek dan Tugas Akhir
+                </h1>
+                <p class="animated slideInDown">Website ini bertujuan untuk memudahkan mahasiswa dan dosen untuk
+                    melakukan pengarsipan laporan dan program kerja praktek dan tugas akhir, serta memudahkan
+                    mahasiswa untuk mencari referensi dari penelitian terdahulu oleh mahasiswa Sistem Informasi. </p>
+                {{-- <a href="" class="btn btn-primary py-3 px-4 animated slideInDown">Login</a> --}}
+            </div>
+            <div class="col-lg-6 animated fadeIn">
+                <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_qp1q7mct.json" background="transparent"
+                    speed="1" style="width: 500px; height: 500;" loop autoplay>
+                </lottie-player>
+                {{-- <iframe src="https://embed.lottiefiles.com/animation/71003" background="transparent" speed="1"
+                    style="width: 700px; height: 200px;"></iframe> --}}
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/landing/main.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>SI-REPO - Repository KP & TGA</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('landing/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('landing/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('landing/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{ asset('landing/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner"
+        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" role="status"></div>
+    </div>
+    <!-- Spinner End -->
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
+        <a href="index.html" class="navbar-brand d-flex align-items-center">
+            <h2 class="m-0 text-primary"><img class="img-fluid me-2" src="img/icon-1.png" alt=""
+                    style="width: 45px;">SI-Repository</h2>
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-4 py-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Kategori</a>
+                    <div class="dropdown-menu shadow-sm m-0">
+                        <a href="#" class="dropdown-item">Repository KP</a>
+                        <a href="#" class="dropdown-item">Repository TGA</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <div class="h-100 d-lg-inline-flex align-items-center d-none">
+
+                {{-- <a class="btn btn-square rounded-circle bg-light text-primary me-2" href=""><i
+                        class="fab fa-instagram"></i></a> --}}
+                <a class="btn btn-outline-primary bg-light text-primary me-0" href="">Login</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Header Start -->
+    @include('landing.header')
+    <!-- Header End -->
+
+    @include('landing.footer')
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
+            class="bi bi-arrow-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('landing/lib/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('landing/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('landing/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('landing/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('landing/lib/counterup/counterup.min.js') }}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{ asset('landing/js/main.js') }}"></script>
+</body>
+
+</html>
